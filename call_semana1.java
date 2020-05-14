ea1c2582660da093f4c9c38220fb5aa1e5640814
Leonardo Dorathoto <leonardod@outlook.com>
2020-05-14 17:53:00 -0300

    java tosco

---
+++ call_semana1.java
@@ -0,0 +1,10 @@
+
+import mypackage.dataUnivesp;
+
+public class call_semana1 {
+    public static void main(String[] args) {
+        mypackage.dataUnivesp  teste = new mypackage.dataUnivesp(2,2,1907);
+
+        System.out.println(teste.toString());
+    }
+}
---
+++ mypackage/dataUnivesp.java
@@ -0,0 +1,57 @@
+package mypackage;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+
+public class dataUnivesp {
+    private int _dia;
+    private int _mes;
+    private int _ano;
+
+    private Date _dataCompleta; 
+
+    //item A
+    public dataUnivesp(int dia, int mes, int ano) {
+        this._dia = dia;
+        this._mes = mes;
+        this._ano = ano;
+        MontaDataCompleta();
+    }
+    public void MontaDataCompleta(){
+        this._dataCompleta = new Date(this._dia, this._mes, this._ano);
+    }
+
+    //Metodos get - item B
+    public int getDia(){
+        return this._dia;
+    }
+    public int getMes(){
+        return this._mes;
+    }
+    public int getAno(){
+        return this._ano;
+    }
+
+    //Item C   - em teoria igual ao construtor
+    public void SetValores(int dia, int mes, int ano) {
+        this._dia = dia;
+        this._mes = mes;
+        this._ano = ano;
+        MontaDataCompleta();
+    }
+
+    //item D
+    //OBS: Esse não é o método mais didático, mas o correto, parse pra date e depois to string
+    public String toString(){
+        return new SimpleDateFormat("dd/MM/yyyy").format(this._dataCompleta);
+    }
+
+    //item E
+    public void trocarCom(Date d1)
+    {
+        this.SetValores(d1.getDate(), d1.getMonth(), d1.getYear());
+    }
+
+}
